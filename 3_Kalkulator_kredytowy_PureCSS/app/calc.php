73c2405f616a59bc77930780efa15053eb7403f1
szymon-marczewski <115935699+szymon-marczewski@users.noreply.github.com>
2023-04-01 15:11:05 +0200

    Add files via upload

--- 3_Kalkulator_kredytowy_PureCSS/app/calc.php
+++ 3_Kalkulator_kredytowy_PureCSS/app/calc.php
@@ -1,1 +1,102 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+//załaduj Smarty
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
 
+//pobranie parametrów
+function getParams(&$form){
+	$form['x'] = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$form['y'] = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$form['pro'] = isset($_REQUEST['pro']) ? $_REQUEST['pro'] : null;	
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
+
+	//sprawdzenie, czy parametry zostały przekazane - jeśli nie to zakończ walidację
+	if ( ! (isset($form['x']) && isset($form['y']) && isset($form['pro']) ))	return false;	
+	
+	//parametry przekazane zatem
+	//nie pokazuj wstępu strony gdy tryb obliczeń (aby nie trzeba było przesuwać)
+	// - ta zmienna zostanie użyta w widoku aby nie wyświetlać całego bloku itro z tłem 
+	$hide_intro = true;
+
+	$infos [] = 'Przekazano parametry.';
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $form['x'] == "") $msgs [] = 'Nie podano kwoty';
+	if ( $form['y'] == "") $msgs [] = 'Nie podano liczby lat';
+	
+	//nie ma sensu walidować dalej gdy brak parametrów
+	if ( count($msgs)==0 ) {
+		// sprawdzenie, czy $x i $y są liczbami całkowitymi
+		if (! is_numeric( $form['x'] )) $msgs [] = 'Pierwsza wartość nie jest liczbą';
+		if (! is_numeric( $form['y'] )) $msgs [] = 'Druga wartość nie jest liczbą';
+	}
+	
+	if (count($msgs)>0) return false;
+	else return true;
+}
+	
+// wykonaj obliczenia
+function process(&$form,&$infos,&$msgs,&$result){
+	$infos [] = 'Parametry poprawne. Wykonuję obliczenia.';
+	
+	//konwersja parametrów na int
+	$form['x'] = floatval($form['x']);
+	$form['y'] = floatval($form['y']);
+	
+	//wykonanie operacji
+	$months = $form['y'] * 12;
+    
+        switch ($form['pro']){
+            case 'low':
+                $procent = 0.15;
+                $monthly = ($form['x'] + ($form['x'] * $procent)) / $months;
+                $result = "Miesięczna rata: ".round($monthly,2)."zł";
+                break;
+                
+            case 'medium':
+                $procent = 0.18;
+                $monthly = ($form['x'] + ($form['x'] * $procent)) / $months;
+                $result = "Miesięczna rata: ".round($monthly,2)."zł";
+                break;
+
+            case 'high':
+                $procent = 0.21;
+                $monthly = ($form['x'] + ($form['x'] * $procent)) / $months;
+                $result = "Miesięczna rata: ".round($monthly,2)."zł";
+                break;   
+       }
+}
+
+//inicjacja zmiennych
+$form = null;
+$infos = array();
+$messages = array();
+$result = null;
+$hide_intro = false;
+	
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro) ){
+	process($form,$infos,$messages,$result);
+}
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Kalkulator Kredytowy');
+$smarty->assign('page_description',' ');
+$smarty->assign('page_header','Kalkulator');
+
+$smarty->assign('hide_intro',$hide_intro);
+
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+// 5. Wywołanie szablonu
+$smarty->display(_ROOT_PATH.'/app/calc.tpl');
